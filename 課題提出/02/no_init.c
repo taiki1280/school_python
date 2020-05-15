5bd79ebfd4ece228a83651d0d4b24a0afd3c441f
taiki <52108527+taiki1280@users.noreply.github.com>
2020-05-15 17:30:20 +0900

    自宅学習

---
+++ 課題提出/02/no_init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char character;
+    int number;
+
+    printf("character is = %c\n", character);
+    printf("number is = %d\n", number);
+
+    return 0;
+}
