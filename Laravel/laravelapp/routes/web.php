bae74f53617e08e15c8f0e6a035bef29e701e4b3
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-09 10:45:52 +0900

    0209,0210

--- Laravel/laravelapp/routes/web.php
+++ Laravel/laravelapp/routes/web.php
@@ -21,38 +21,4 @@ Route::get('/', function () {
 //   return view('hello');
 // });
 
-Route::get('hello/{msg?}', 'HelloController@index');
-// Route::get('hello/{msg?}', function ($msg = 'メッセージ内容はありません') {
-//   $html = <<<EOF
-//     <html>
-//     <head>
-//       <title>Hello</title>
-//       <style>
-//         body {
-//           font-size: 16pt;
-//           color: #999;
-//         }
-//         h1 {
-//           font-size: 100pt;
-//           text-align: right;
-//           margin: -40px 0px -50px 0px;
-//           color: #eee;
-//         }
-//         h2 {
-//           font-size: 80pt;
-//           text-align: center;
-//           margin: auto;
-//           color: #955
-//         }
-//       </style>
-//     </head>
-//     <body>
-//       <h1>Hello</h1>
-//       <h2>{$msg}</h2>
-//       <p>This is samplepage.</p>
-//       <p>これは、サンプルで作ったページです。</p>
-//     </body>
-//     </html>
-//   EOF;
-//   return $html;
-// });
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
