2c672b020aa4490c3e626cc2fb9c24fe52046947
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-10 09:53:04 +0900

    0307-0309

--- Laravel/laravelapp/resources/views/hello/index.php
+++ Laravel/laravelapp/resources/views/hello/index.php
@@ -1,5 +1,5 @@
 <body>
   <h1>Index</h1>
   <p><?php echo $msg; ?></p>
-  <p><?php echo date("Y年n月j日"); ?></p>
+  <p>ID=<?php echo $id; ?></p>
 </body>
--- Laravel/laravelapp/routes/web.php
+++ Laravel/laravelapp/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello/{id?}', 'HelloController@index');
 
 Route::get('jissyu', 'JissyuController@index');
