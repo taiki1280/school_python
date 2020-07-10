d2dbd6f68f96115f93f9af19b38d8ccbab660d96
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-10 11:34:20 +0900

    0324

--- Laravel/laravelapp/resources/views/hello/index.blade.php
+++ Laravel/laravelapp/resources/views/hello/index.blade.php
@@ -1,11 +1,15 @@
 <body>
   <h1>Blade/Index</h1>
   <p>&#064;forディレクティブの例</p>
-  <ol>
-    @for ($i = 1;$i < 100;$i++) @if ($i % 2==1) @continue @elseif ($i <=10) <li>No, {{$i}}
-      @else
-      @break
-      @endif
-      @endfor
-  </ol>
+  @foreach ($data as $item)
+  @if ($loop->first)
+  <p>※データ一覧</p>
+  <ul>
+    @endif
+    <li>No,{{$loop->iteration}}. {{$item}}</li>
+    @if ($loop->last)
+  </ul>
+  <p>――ここまで</p>
+  @endif
+  @endforeach
 </body>
