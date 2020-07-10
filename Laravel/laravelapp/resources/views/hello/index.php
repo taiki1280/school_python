9f2665a1b1ca17b46d05177d36b1841872df21dd
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-10 09:50:28 +0900

    0305,0306

--- Laravel/laravelapp/resources/views/hello/index.php
+++ Laravel/laravelapp/resources/views/hello/index.php
@@ -1,25 +1,5 @@
-<html>
-
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {
-      font-size: 16pt;
-      color: #999;
-    }
-
-    h1 {
-      font-size: 100pt;
-      text-align: right;
-      color: #f6f6f6;
-      margin: -50px 0px -100px 0px;
-    }
-  </style>
-</head>
-
 <body>
   <h1>Index</h1>
-  <p>This is a sample page with php-template.</p>
+  <p><?php echo $msg; ?></p>
+  <p><?php echo date("Y年n月j日"); ?></p>
 </body>
-
-</html>
