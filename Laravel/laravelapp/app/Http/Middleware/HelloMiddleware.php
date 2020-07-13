82d92fbb000a6a2970ce37163a7ad70973d51ca1
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-13 09:38:52 +0900

    0402-0405

--- Laravel/laravelapp/app/Http/Middleware/HelloMiddleware.php
+++ Laravel/laravelapp/app/Http/Middleware/HelloMiddleware.php
@@ -6,15 +6,14 @@ use Closure;
 
 class HelloMiddleware
 {
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   * @return mixed
-   */
   public function handle($request, Closure $next)
   {
+    $data = [
+      ['name' => 'taro', 'mail' => 'taro@yamada'],
+      ['name' => 'hanako', 'mail' => 'hanako@flower'],
+      ['name' => 'sachiko', 'mail' => 'sachico@happy'],
+    ];
+    $request->merge(['data' => $data]);
     return $next($request);
   }
 }
