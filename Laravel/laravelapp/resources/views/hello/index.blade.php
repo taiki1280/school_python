3d6fbc909183e50accc625d62c12aa77d000b84b
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-13 09:44:07 +0900

    0406-0408

--- Laravel/laravelapp/resources/views/hello/index.blade.php
+++ Laravel/laravelapp/resources/views/hello/index.blade.php
@@ -9,16 +9,9 @@
 
 @section('content')
 <p>ここが本文のコンテンツです。</p>
-<table>
-  @foreach($data as $item)
-  <tr>
-    <th>{{$item['name']}}</th>
-    <td>{{$item['mail']}}</td>
-  </tr>
-  @endforeach
-</table>
+<p>これは、<middleware>google.com</middleware>へのリンクです。</p>
+<p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
 @endsection
-
 
 @section('footer')
 copyright 2020 tuyano.
