bb026c9941d0073ea712c243ecd2e582439d5566
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-13 13:10:59 +0900

    実習4

--- Laravel/laravelapp/resources/views/jissyu3_3/index.blade.php
+++ Laravel/laravelapp/resources/views/jissyu3_3/index.blade.php
@@ -1,33 +1,39 @@
-<!-- extends('___(4)___') -->
 @extends('layouts.jissyu')
 
-<!-- section('title', '___(5)___') -->
 @section('title', 'Jissyu')
+<!-- ___(1)___('___(2)___') -->
 
-<!-- ___(6)___('menu_title') -->
-@section('menu_title')
-Laravelについて
-<!-- ___(7)___ -->
-@endsection
 
-<!-- section('___(8)___') -->
 @section('menu_content')
 <ul>
   <li>特徴</li>
   <li>バージョン</li>
 </ul>
+@component('components.menu')
+<!-- ___(3)___('menu_title') -->
+@slot('menu_title')
+特徴
+<!-- ___(4)___ -->
+@endslot
+
+@slot('menu_content')
+<p>Laravelは、MVCのWebアプリケーション開発用の無料・オープンソースのPHPで書かれたWebアプリケーションフレームワークである。</p>
+@endslot
+<!-- ___(5)___ -->
+@endcomponent
 @endsection
 
-<!-- section('___(9)___') -->
 @section('content')
 <p>開発元：Taylor Otwell</p>
 <p>初版：2011年6月</p>
 <p>プログラミング言語：PHP</p>
 <p>対応OS：クロスプラットフォーム</p>
 <p>公式サイト：laravel.com</p>
+<!-- ___(6)___('___(7)___', ['subview_title'=>'バージョン', -->
+@include('components.subview', ['subview_title'=>'バージョン',
+'subview_content'=>'2020年3月より最新バージョンは7。PHPは7.2.5以上。'])
 @endsection
 
 @section('footer')
-<!-- ___(10)___ -->
 copyright 2020 東京情報クリエイター工学院専門学校
 @endsection
