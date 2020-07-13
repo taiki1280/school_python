9b6c6243479e3d9a546ac1d0e4e887456fe31915
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-13 09:50:02 +0900

    0412,0413

--- Laravel/laravelapp/routes/web.php
+++ Laravel/laravelapp/routes/web.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')
+  ->middleware('helo');
 
 Route::post('hello', 'HelloController@post');
 
