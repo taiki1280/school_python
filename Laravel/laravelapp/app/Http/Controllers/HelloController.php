07dd869925a6275075387a90f150a1d81528a871
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-14 10:29:30 +0900

    0424

--- Laravel/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel/laravelapp/app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -11,8 +12,19 @@ class HelloController extends Controller
   {
     return view('hello.index', ['msg' => 'フォームを入力：']);
   }
-  public function post(HelloRequest $request)
+
+  public function post(Request $request)
   {
+    $validator = Validator::make($request->all(), [
+      'name' => 'required',
+      'mail' => 'email',
+      'age' => 'numeric|between:0,150',
+    ]);
+    if ($validator->fails()) {
+      return redirect('/hello')
+        ->withErrors($validator)
+        ->withInput();
+    }
     return view('hello.index', ['msg' => '正しく入力されました！']);
   }
 }
