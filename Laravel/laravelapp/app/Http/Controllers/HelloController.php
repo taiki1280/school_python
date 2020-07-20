684fe96134ef8b9b3edf51f22ae9342468f622ce
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-20 11:25:33 +0900

    0507

--- Laravel/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel/laravelapp/app/Http/Controllers/HelloController.php
@@ -7,25 +7,17 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    $items = DB::select('select * from people');
+    if (isset($request->id)) {
+      $param = ['id' => $request->id];
+      $items = DB::select(
+        'select * from people where id = :id',
+        $param
+      );
+    } else {
+      $items = DB::select('select * from people');
+    }
     return view('hello.index', ['items' => $items]);
-  }
-
-  public function post(Request $request)
-  {
-    $validate_rule = [
-      'msg' => 'required',
-    ];
-    $this->validate($request, $validate_rule);
-    $msg = $request->msg;
-    $response = response()->view(
-      'hello.index',
-      ['msg' => '「' . $msg .
-        '」をクッキーに保存しました。']
-    );
-    $response->cookie('msg', $msg, 100);
-    return $response;
   }
 }
