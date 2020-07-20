7e15b368d67b79f9378bc7943f9cb34d1d590bd7
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-20 12:08:58 +0900

    0514-0516

--- Laravel/laravelapp/resources/views/hello/index.blade.php
+++ Laravel/laravelapp/resources/views/hello/index.blade.php
@@ -9,9 +9,15 @@
 
 @section('content')
   <table>
-  <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+  <tr>
+    <th>id</th>
+    <th>Name</th>
+    <th>Mail</th>
+    <th>Age</th>
+  </tr>
   @foreach ($items as $item)
     <tr>
+      <td>{{$item->id}}</td>
       <td>{{$item->name}}</td>
       <td>{{$item->mail}}</td>
       <td>{{$item->age}}</td>
