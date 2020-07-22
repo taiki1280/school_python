db976f3b55fde76ff1106413f60b8632ffba7a21
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-22 14:04:32 +0900

    0701

--- Laravel/laravelapp/database/migrations/2020_07_22_045520_create_restdata_table.php
+++ Laravel/laravelapp/database/migrations/2020_07_22_045520_create_restdata_table.php
@@ -1,31 +1,26 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateRestdataTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('restdata', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  public function up()
+  {
+    Schema::create(
+      'restdata',
+      function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('message');
+        $table->string('url');
+        $table->timestamps();
+      }
+    );
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('restdata');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('restdata');
+  }
 }
