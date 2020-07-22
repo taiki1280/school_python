cd882e268a44d763657e094a567b754425578db2
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-22 14:15:48 +0900

    なんかのミス

--- Laravel/laravelapp/resources/views/person/index.blade.php
+++ Laravel/laravelapp/resources/views/person/index.blade.php
@@ -34,19 +34,6 @@
   </table>
 @endsection
 
-
-
-@section('content')
-  <table>
-  <tr><th>Data</th></tr>
-  @foreach ($items as $item)
-    <tr>
-      <td>{{$item->getData()}}</td>
-    </tr>
-  @endforeach
-  </table>
-@endsection
-
 @section('footer')
 copyright 2020 tuyano.
 @endsection
