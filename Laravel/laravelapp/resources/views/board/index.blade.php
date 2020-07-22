9071b21523b5b4611461aab02c6932854aba5139
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-22 14:02:24 +0900

    0643

--- Laravel/laravelapp/resources/views/board/index.blade.php
+++ Laravel/laravelapp/resources/views/board/index.blade.php
@@ -9,14 +9,19 @@
 
 @section('content')
   <table>
-  <tr><th>Data</th></tr>
-  @foreach ($items as $item)
     <tr>
-      <td>{{$item->getData()}}</td>
+      <th>Message</th>
+      <th>Name</th>
     </tr>
-  @endforeach
+    @foreach ($items as $item)
+    <tr>
+      <td>{{$item->message}}</td>
+      <td>{{$item->person->name}}</td>
+    </tr>
+    @endforeach
   </table>
 @endsection
+
 
 @section('footer')
 copyright 2020 tuyano.
