cace4ec531b10b4bc51b5f12e5e5e1260499e914
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-22 09:42:40 +0900

    ミス

--- Laravel/laravelapp/resources/views/person/index.blade.php
+++ Laravel/laravelapp/resources/views/person/index.blade.php
@@ -1,26 +1,21 @@
 @extends('layouts.helloapp')
 
-@section('title', 'Person.find')
+@section('title', 'Person.index')
 
 @section('menubar')
   @parent
-  検索ページ
+  インデックスページ
 @endsection
 
 @section('content')
-  <form action="/person/find" method="post">
-  @csrf
-  <input type="text" name="input" value="{{$input}}">
-  <input type="submit" value="find">
-  </form>
-  @if (isset($item))
   <table>
   <tr><th>Data</th></tr>
-  <tr>
-    <td>{{$item->getData()}}</td>
-  </tr>
+  @foreach ($items as $item)
+    <tr>
+      <td>{{$item->getData()}}</td>
+    </tr>
+  @endforeach
   </table>
-  @endif
 @endsection
 
 @section('footer')
