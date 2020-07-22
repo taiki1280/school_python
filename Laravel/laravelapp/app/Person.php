f9246bfec8504c4e720db31aa06769df1c6f4cc6
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-22 10:42:59 +0900

    0637

--- Laravel/laravelapp/app/Person.php
+++ Laravel/laravelapp/app/Person.php
@@ -22,8 +22,8 @@ class Person extends Model
     return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
   }
 
-  public function board()
+  public function boards()
   {
-    return $this->hasOne('App\Board');
+    return $this->hasMany('App\Board');
   }
 }
