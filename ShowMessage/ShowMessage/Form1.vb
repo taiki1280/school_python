c744727be35eeeb07e8330d9c5ede68571d10150
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-27 19:29:08 +0900

    058_終了イベント

--- ShowMessage/ShowMessage/Form1.vb
+++ ShowMessage/ShowMessage/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub ShowMessage(sender As Object, e As EventArgs) Handles btnMessage.Click
         lblMessage.Text = "Hello VB!"
     End Sub
+
+    Private Sub ExitProc(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
 End Class
