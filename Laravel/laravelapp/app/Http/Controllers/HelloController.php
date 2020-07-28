27b5bbd6a19e5c73492c190a85c320411cfaae90
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-28 11:59:47 +0900

    0722,0723

--- Laravel/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel/laravelapp/app/Http/Controllers/HelloController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Person;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    $items = DB::table('people')->simplePaginate(5);
-    return view('hello.index', ['items' => $items]);
+    $sort = $request->sort;
+    $items = Person::orderBy($sort, 'asc')
+      ->simplePaginate(5);
+    $param = ['items' => $items, 'sort' => $sort];
+    return view('hello.index', $param);
   }
 
   public function post()
