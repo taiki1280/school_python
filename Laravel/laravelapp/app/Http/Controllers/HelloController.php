954b828828f7638e71b68dbda007cb62cc0f5987
taiki <52108527+taiki1280@users.noreply.github.com>
2020-07-28 12:21:46 +0900

    0729

--- Laravel/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel/laravelapp/app/Http/Controllers/HelloController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Person;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HelloController extends Controller
 {
   public function index(Request $request)
   {
+    $user = Auth::user();
     $sort = $request->sort;
     $items = Person::orderBy($sort, 'asc')
-      ->paginate(5);
-    $param = ['items' => $items, 'sort' => $sort];
+      ->simplePaginate(5);
+    $param = ['items' => $items, 'sort' => $sort, 'user' => $user];
     return view('hello.index', $param);
   }
 
