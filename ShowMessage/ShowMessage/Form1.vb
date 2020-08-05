2a1f06aa8a9bc2e2f7f904796f0106759b4aed5c
taiki <52108527+taiki1280@users.noreply.github.com>
2020-08-05 09:51:05 +0900

    079_複数行のステートメント(コード)

--- ShowMessage/ShowMessage/Form1.vb
+++ ShowMessage/ShowMessage/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class fmtShowMessage
-    Private Sub ShowMessage(sender As Object, e As EventArgs) Handles btnMessage.Click
+    Private Sub ShowMessage(sender As Object,
+                            e As EventArgs) Handles btnMessage.Click
         'lblMessage.Text = "Hello VB!"'
         'メッセージボックスに日付を表示します。'
         MessageBox.Show(#3/14/2020#)    '#で囲むと日付型のリテラル
