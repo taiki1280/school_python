16592ba07e8df139adcaf0885395c2c6509bd6fa
taiki <52108527+taiki1280@users.noreply.github.com>
2020-08-05 10:07:26 +0900

    091_変数のスコープ

--- ShowMessage/ShowMessage/Form1.Designer.vb
+++ ShowMessage/ShowMessage/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class fmtShowMessage
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,57 +20,63 @@ Partial Class fmtShowMessage
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblMessage = New System.Windows.Forms.Label()
-        Me.btnMessage = New System.Windows.Forms.Button()
-        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ShowString1 = New System.Windows.Forms.Button()
+        Me.ShowString2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'lblMessage
+        'ShowString1
         '
-        Me.lblMessage.AutoSize = True
-        Me.lblMessage.Location = New System.Drawing.Point(33, 40)
-        Me.lblMessage.Name = "lblMessage"
-        Me.lblMessage.Size = New System.Drawing.Size(0, 18)
-        Me.lblMessage.TabIndex = 0
+        Me.ShowString1.Location = New System.Drawing.Point(91, 124)
+        Me.ShowString1.Name = "ShowString1"
+        Me.ShowString1.Size = New System.Drawing.Size(153, 51)
+        Me.ShowString1.TabIndex = 0
+        Me.ShowString1.Text = "Button1"
+        Me.ShowString1.UseVisualStyleBackColor = True
         '
-        'btnMessage
+        'ShowString2
         '
-        Me.btnMessage.Location = New System.Drawing.Point(64, 99)
-        Me.btnMessage.Name = "btnMessage"
-        Me.btnMessage.Size = New System.Drawing.Size(147, 46)
-        Me.btnMessage.TabIndex = 1
-        Me.btnMessage.Text = "表示(&S)"
-        Me.btnMessage.UseVisualStyleBackColor = True
-        '
-        'btnExit
-        '
-        Me.btnExit.Location = New System.Drawing.Point(270, 99)
-        Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(147, 46)
-        Me.btnExit.TabIndex = 2
-        Me.btnExit.Text = "終了(&X)"
-        Me.btnExit.UseVisualStyleBackColor = True
+        Me.ShowString2.Location = New System.Drawing.Point(368, 124)
+        Me.ShowString2.Name = "ShowString2"
+        Me.ShowString2.Size = New System.Drawing.Size(153, 51)
+        Me.ShowString2.TabIndex = 1
+        Me.ShowString2.Text = "Button2"
+        Me.ShowString2.UseVisualStyleBackColor = True
         '
         'fmtShowMessage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(481, 202)
-        Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.btnMessage)
-        Me.Controls.Add(Me.lblMessage)
+        Me.ClientSize = New System.Drawing.Size(607, 289)
+        Me.Controls.Add(Me.ShowString2)
+        Me.Controls.Add(Me.ShowString1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "fmtShowMessage"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblMessage As Label
-    Friend WithEvents btnMessage As Button
-    Friend WithEvents btnExit As Button
+    Friend WithEvents ShowString1 As Button
+    Friend WithEvents ShowString2 As Button
+
+    Dim x As String = "外側のx"
+    Private Sub ShowString1_Click(sender As Object, e As EventArgs) Handles ShowString1.Click
+        Dim x As String = "内側のx"
+
+        Dim y As String = "内側のy"
+        '内側のx
+        Debug.WriteLine(x)
+    End Sub
+
+    Private Sub ShowString2_Click(sender As Object, e As EventArgs) Handles ShowString2.Click
+        '外側のx
+        Debug.WriteLine(x)
+
+        '存在しない
+        'Debug.WriteLine(y)
+
+    End Sub
 End Class
