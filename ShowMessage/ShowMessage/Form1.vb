5ddfb6cd36430b9c5c6d4f24114cc17d817b671e
taiki <52108527+taiki1280@users.noreply.github.com>
2020-08-05 09:48:49 +0900

    078_メッセージボックスとコメント

--- ShowMessage/ShowMessage/Form1.vb
+++ ShowMessage/ShowMessage/Form1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class fmtShowMessage
     Private Sub ShowMessage(sender As Object, e As EventArgs) Handles btnMessage.Click
-        lblMessage.Text = "Hello VB!"
+        'lblMessage.Text = "Hello VB!"'
+        'メッセージボックスに日付を表示します。'
+        MessageBox.Show(#3/14/2020#)    '#で囲むと日付型のリテラル
     End Sub
 
     Private Sub ExitProc(sender As Object, e As EventArgs) Handles btnExit.Click
