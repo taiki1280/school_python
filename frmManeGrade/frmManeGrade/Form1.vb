80a537aeef03e6acce5575ed668983e4ced61215
taiki <52108527+taiki1280@users.noreply.github.com>
2020-08-06 17:38:17 +0900

    Update Form1.vb

--- frmManeGrade/frmManeGrade/Form1.vb
+++ frmManeGrade/frmManeGrade/Form1.vb
@@ -1,23 +1,23 @@
 ﻿Imports System.Threading
 Public Class frmManeGrade
-    Public Function Check(value)
-        If value >= 60 Then
-            Return "合"
-        Else
-            Return "否"
-        End If
-    End Function
     Private Sub InitProc(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim points As New Dictionary(Of String, Integer)()
-        points.Add("基本情報", 60)
-        points.Add("HTML", 60)
-        points.Add("JavaScript", 40)
-        points.Add("Java", 80)
-        points.Add("Python", 50)
-        points.Add("オブジェクト指向", 90)
-        For i As Integer = 0 To 5
-            lstScore.Items.Add(String.Format("{0} {1} {2}", points.Keys(i), points(points.Keys(i)), Check(points(points.Keys(i)))))
+        Dim points As New Dictionary(Of String, Integer) From
+        {
+            {"基本情報", 60},
+            {"HTML", 60},
+            {"JavaScript", 40},
+            {"Java", 80},
+            {"Python", 50},
+            {"オブジェクト指向", 90}
+        }
+        Dim average As Integer = 0
+        For i As Integer = 0 To points.Count() - 1
+            average += points.Values(i)
+            lstScore.Items.Add(String.Format("{0} {1} {2}", points.Keys(i), points.Values(i), If(points.Values(i) >= 60, "合格", "否")))
         Next
+        average /= points.Count()
+        lstScore.Items.Add("")
+        lstScore.Items.Add(String.Format("平均点　{0}", average))
     End Sub
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Application.Exit()
