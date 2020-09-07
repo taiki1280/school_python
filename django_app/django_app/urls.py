8f5676f99be91d285bb56efe62264115f9c40b9a
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-07 10:56:01 +0900

    list2-4

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+import hello.views as hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/hello', include('/hello')),
+    path('hello/', hello.index),
 ]
