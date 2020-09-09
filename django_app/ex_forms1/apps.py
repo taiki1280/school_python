e8296a79f031165ef3ed52196537e3d36d845d03
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-09 12:13:18 +0900

    課題0301

---
+++ django_app/ex_forms1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExForms1Config(AppConfig):
+    name = 'ex_forms1'
---
+++ django_app/ex_forms1/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django_app import urls
+
+
+def index(request):
+    tmp = {
+        "title": "ex_forms1/index",
+        "subtitle": "ex_forms1/index",
+        "msg": "文字列を入力してください"
+    }
+    return render(request, 'ex_forms1/index.html', tmp)
+    # return render(request, 'base.html')
+
+
+def form(request):
+    msg = request.POST["msg"]
+    tmp = {
+        "title": "ex_forms1/form",
+        "subtitle": "ex_forms1/form",
+        "msg": f'入力文字列 = {msg}、長さ = {len(msg)}'
+    }
+    return render(request, 'ex_forms1/index.html', tmp)
