fade5c37764024b6762db17a657c3ece44cb8b0b
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-10 10:34:28 +0900

    完成

--- django_app/ex_forms4/forms.py
+++ django_app/ex_forms4/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class Form(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(Form, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
     name = forms.CharField(label='県名')
     population = forms.IntegerField(label='県人口')
     prefecture = forms.CharField(label='県庁所在地')
--- django_app/ex_forms4/views.py
+++ django_app/ex_forms4/views.py
@@ -18,12 +18,12 @@ class View(TemplateView):
         msg = []
         name = f'{request.POST["name"]}'
         population = f'{request.POST["population"]}万人'
-        prefecture = f'{request.POST["prefecture"]}市'
+        prefecture = f'{request.POST["prefecture"]}'
         population_prefecture = f'{request.POST["population_prefecture"]}万人'
-        msg += [("県名",name)]
-        msg += [("県人口",population)]
-        msg += [("県庁所在地",prefecture)]
-        msg += [("県庁所在地人口",population_prefecture)]
+        msg += [("県名", name)]
+        msg += [("県人口", population)]
+        msg += [("県庁所在地", prefecture)]
+        msg += [("県庁所在地人口", population_prefecture)]
         self.tmp = {
             "title": "県情報",
             "subtitle": "県情報",
