072a828820e42a909fed5a9a90b2b91370b51232
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-11 10:33:32 +0900

    @修正

--- django_app/ex_forms5/forms.py
+++ django_app/ex_forms5/forms.py
@@ -4,10 +4,9 @@ from django import forms
 class Form(forms.Form):
     name = forms.CharField(
         label='name', widget=forms.TextInput(attrs={"class": "form-control"}))
-    email = forms.EmailField(label='email')
-    # @存在エラーが出ない
-    # email = forms.EmailField(
-    #     label='email', widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(
+        label='email',
+        widget=forms.EmailInput(attrs={"class": "form-control"}))
     Windows10 = forms.BooleanField(label='Windows10', required=False)
     Mac = forms.BooleanField(label='Mac', required=False)
     Linux = forms.BooleanField(label='Linux', required=False)
