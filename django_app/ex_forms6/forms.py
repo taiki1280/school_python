209318825c0257c132e055266a86ede06bc32d59
taiki <20001208taiki@gmail.com>
2020-09-12 23:55:33 +0900

    完成

--- django_app/ex_forms6/forms.py
+++ django_app/ex_forms6/forms.py
@@ -2,10 +2,12 @@ from django import forms
 
 
 class Form(forms.Form):
-    name = forms.CharField(
+    商品名 = forms.CharField(
         label='商品名', widget=forms.TextInput(attrs={"class": "form-control"}))
-    num = forms.EmailField(
-        label='数量', widget=forms.EmailInput(attrs={"class": "form-control"}))
-    Windows10 = forms.BooleanField(label='領収書の有無, required=False)
-    Mac = forms.BooleanField(label='Mac', required=False)
-    Linux = forms.BooleanField(label='Linux', required=False)
+    # choices = [(v+1, v+1) for v in range(10)]
+    個数 = forms.ChoiceField(
+        label='数量', widget=forms.Select, choices=[(v+1, v+1) for v in range(10)])
+    # label='数量', widget=forms.Select, choices=choices)
+    領収書の有無 = forms.BooleanField(label='領収書の有無', required=False)
+    送付方法 = forms.ChoiceField(
+        label='送付方法', widget=forms.RadioSelect, choices=[(v, v) for v in ["宅配便", "メール便", "冊子小包"]])
--- django_app/ex_forms6/views.py
+++ django_app/ex_forms6/views.py
@@ -7,7 +7,19 @@ from .forms import Form
 class View(TemplateView):
     def get(self, request):
         self.tmp = {"title": "商品発送", "form": Form()}
-        return render(request, 'ex_forms5/index.html', self.tmp)
+        return render(request, 'ex_forms6/index.html', self.tmp)
 
     def post(self, request):
-        return render(request, 'ex_forms5/index.html', self.tmp)
+        forms = ["商品名", "個数", "領収書の有無", "送付方法"]
+        form = {}
+        for v in forms:
+            form[v] = ""
+        for v in forms:
+            if v in request.POST:
+                form[v] = request.POST[v]
+        if form["領収書の有無"] == "on":
+            form["領収書の有無"] = "有"
+        else:
+            form["領収書の有無"] = "無"
+        tmp = {"title": "商品発送", "form": form}
+        return render(request, 'ex_forms6/index.html', tmp)
