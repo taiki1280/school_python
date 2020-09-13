e7aff7fe69b92c8f9b858fdd6ffa000734dc0773
taiki <20001208taiki@gmail.com>
2020-09-13 22:15:19 +0900

    0220

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     params = {
-        'title':'Hello/Index',
-        'msg':'これは、サンプルで作ったページです。',
+        'title': 'Hello/Index',
+        'msg': 'これは、サンプルで作ったページです。',
+        'goto': 'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'これは、もう１つのページです。',
+        'goto': 'index',
     }
     return render(request, 'hello/index.html', params)
