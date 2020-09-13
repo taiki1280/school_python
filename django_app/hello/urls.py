46bf4c6c4ee25ccfd25293c4c385d1cda9e9dc9e
taiki <20001208taiki@gmail.com>
2020-09-13 21:54:54 +0900

    0205

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-import hello.views as hello
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello.index),
+    path('', views.index, name='index'),
 ]
