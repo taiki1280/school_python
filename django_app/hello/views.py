24803d96bc122261c8601dedeee0b3de3a336aeb
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-14 09:52:53 +0900

    0449

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -13,6 +13,10 @@ class HelloView(TemplateView):
 
     def post(self, request):
         ch = request.POST.getlist('choice')
-        self.params['result'] = 'selected: ' + str(ch) + '.'
+        result = '<ol class="list-group"><b>selected:</b>'
+        for item in ch:
+            result += '<li class="list-group-item">' + item + '</li>'
+        result += '</ol>'
+        self.params['result'] = result
         self.params['form'] = HelloForm(request.POST)
         return render(request, 'hello/index.html', self.params)
