f203bf6151308b4973030588a6ea89d3a08fbdc0
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-14 09:36:38 +0900

    0238

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -2,9 +2,4 @@ from django import forms
 
 
 class HelloForm(forms.Form):
-    name = forms.CharField(label='name', \
-        widget=forms.TextInput(attrs={'class':'form-control'}))
-    mail = forms.CharField(label='mail', \
-        widget=forms.TextInput(attrs={'class':'form-control'}))
-    age = forms.IntegerField(label='age', \
-        widget=forms.NumberInput(attrs={'class':'form-control'}))
+    check = forms.BooleanField(label='Checkbox', required=False)
