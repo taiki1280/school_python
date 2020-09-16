1c8576c3361e150e3d15ad0a6685af111e13ece3
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-16 10:15:26 +0900

    課題02

--- ex_db_app/ex_db1/views.py
+++ ex_db_app/ex_db1/views.py
@@ -4,9 +4,15 @@ from .models import Book
 
 
 def index(request):
-    params = {
-        'title': 'Hello',
-        'message': 'all friends.',
-        'data': Book.objects.all(),
-    }
-    return render(request, 'ex_db1/index.html', params)
+    if request.method == 'GET':
+        params = {
+            'title': '図書管理',
+            'message': '全図書',
+            'data': Book.objects.all(),
+        }
+        return render(request, 'ex_db1/index.html', params)
+    elif request.method == 'POST':
+        id_num = request.POST['ID']
+        data = Book.objects.filter(id=id_num)
+        params = {'title': '図書管理検索結果', 'message': '全図書', 'data': data}
+        return render(request, 'ex_db1/index.html', params)
