a27b67fa49a4d4b116c877c95e1e3a02bfe9b671
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-18 10:38:38 +0900

    課題0305

--- ex_db_app/ex_db2/forms.py
+++ ex_db_app/ex_db2/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 
 
-class IdForm(forms.Form):
-    ID = forms.IntegerField(widget=forms.NumberInput,
-                            label='ID',
-                            required=False)
-class AuthorForm(forms.Form):
-    author = forms.CharField(widget=forms.TextInput,
-                            label='author',
-                            required=False)
+class Form(forms.Form):
+    # value = forms.IntegerField(widget=forms.NumberInput,
+    #                            label='value',
+    #                            required=False)
+    value = forms.Field(widget=forms.TextInput, label='value', required=False)
--- ex_db_app/ex_db2/views.py
+++ ex_db_app/ex_db2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Book
-from .forms import IdForm,AuthorForm
+from .forms import Form
 
 
 def index(request):
@@ -11,23 +11,34 @@ def index(request):
     }
     if request.method == 'GET':
         params['data'] = Book.objects.all()
-        params['id_form'] = IdForm()
-        params['author_form'] = AuthorForm()
+        params['form'] = Form()
         return render(request, 'ex_db2/index.html', params)
     elif request.method == 'POST':
-        params['id_form'] = IdForm(request.POST)
-        params['author_form'] = AuthorForm(request.POST)
-        id_num = request.POST['ID']
-        # if id_num != '':
-            # 検索結果無の時にも対応している
-            # data = Book.objects.filter(id=id_num)
-            # 配列にしなければならないかつ、検索結果無の時はエラー
-            # data = [Book.objects.get(id=id_num)]
+        params['form'] = Form(request.POST)
+        value = request.POST['value']
+        # if value != '':
+        # 検索結果無の時にも対応している
+        # data = Book.objects.filter(id=value)
+        # 配列にしなければならないかつ、検索結果無の時はエラー
+        # data = [Book.objects.get(id=value)]
         # else:
         #     data = Book.objects.all()
         # 三項演算子
+        # data = Book.objects.filter(
+        #     id=value) if value != '' else Book.objects.all()
+
+        # 書名で完全一致検索
+        # data = Book.objects.filter(
+        #     title=value) if value != '' else Book.objects.all()
+
+        # 著書名で曖昧一致検索
+        # data = Book.objects.filter(
+        #     author__contains=value) if value != '' else Book.objects.all()
+
+        # 価格による値よりも等しいか小さい
         data = Book.objects.filter(
-            id=id_num) if id_num != '' else Book.objects.all()
+            price__lte=value) if value != '' else Book.objects.all()
+
         params['title'] += '検索結果'
         params['data'] = data
         return render(request, 'ex_db2/index.html', params)
