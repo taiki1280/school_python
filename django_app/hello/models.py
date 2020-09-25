9796f1645cacfffdd881b64a0797c699d05bf322
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 12:12:51 +0900

    0422

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Friend(models.Model):
     name = models.CharField(max_length=100)
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(validators=[ \
+          MinValueValidator(0), \
+          MaxValueValidator(150)])
     birthday = models.DateField()
 
     def __str__(self):
-        return f'name:{self.name}'
+        return f'id:{self.id} name:{self.name}'
