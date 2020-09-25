1c91bc1f685cce084111f6003f1242234c9ce018
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 09:51:38 +0900

    0314

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .forms import HelloForm
 from .models import Friend
+from .forms import HelloForm
 
 
-class HelloView(TemplateView):
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'message': 'all friends.',
-            'data': Friend.objects.all(),
-        }
-
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
+def index(request):
+    params = {
+        'title': 'Hello',
+        'message': 'all friends.',
+        'form': HelloForm(),
+        'data': [],
+    }
+    if (request.method == 'POST'):
+        num = request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
+    return render(request, 'hello/index.html', params)
