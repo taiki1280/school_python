4a8ea60c1f87baaa92789f0c1d151b62148aa5da
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 12:19:45 +0900

    0425

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, URLValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, URLValidator, RegexValidator
 
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[URLValidator()])
+        validators=[RegexValidator(r'^[a-z]*$')])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
