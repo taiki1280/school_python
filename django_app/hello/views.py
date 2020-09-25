fc88accd40ad4de489892b331e6b12e050b76826
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:36:00 +0900

    0329

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Friend
-from .forms import HelloForm
+# from .forms import HelloForm
+from .forms import FriendForm
 
 
 def index(request):
@@ -15,18 +16,13 @@ def index(request):
 
 # create model
 def create(request):
-    params = {
-        'title': 'Hello',
-        'form': HelloForm(),
-    }
     if (request.method == 'POST'):
-        name = request.POST['name']
-        mail = request.POST['mail']
-        gender = 'gender' in request.POST
-        age = int(request.POST['age'])
-        birth = request.POST['birthday']
-        friend = Friend(name=name,mail=mail,gender=gender,\
-                age=age,birthday=birth)
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
         friend.save()
         return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
     return render(request, 'hello/create.html', params)
