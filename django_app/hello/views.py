44930ad67d3a32601fc4acdc5c4af36914debbe2
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:19:27 +0900

    0321

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from .models import Friend
+from django.db.models import QuerySet
+
+
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+
+QuerySet.__str__ = __new_str__
 
 
 def index(request):
-    num = Friend.objects.all().count()
-    first = Friend.objects.all().first()
-    last = Friend.objects.all().last()
-    data = [num, first, last]
+    data = Friend.objects.all().values('id', 'name', 'age')
     params = {
         'title': 'Hello',
         'data': data,
