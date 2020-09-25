f0b3f302d8018982ff6f195193d57aa8e329d0b4
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 12:21:19 +0900

    0426

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,14 +1,24 @@
+import re
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, URLValidator, RegexValidator
+from django.core.validators import ValidationError
+
+
+def number_only(value):
+    if (re.match(r'^[0-9]*$', value) == None):
+        raise ValidationError(
+            '%(value)s is not Number!', \
+            params={'value': value},
+        )
 
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[RegexValidator(r'^[a-z]*$')])
+        validators=[number_only])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
     birthday = models.DateField()
 
     def __str__(self):
-        return f'id:{self.id} name:{self.name}'
+        return '<Friend:id=' + str(self.id) + ', ' + \
+            self.name + '(' + str(self.age) + ')>'
