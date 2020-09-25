e609253c00ac0d36690cefdb09d212d1fb3c7a93
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:43:03 +0900

    0334

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -26,3 +26,17 @@ def create(request):
         'form': FriendForm(),
     }
     return render(request, 'hello/create.html', params)
+
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
