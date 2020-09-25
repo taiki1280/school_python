903aa132bc1e2fd318072ce5f9a76af65fe44598
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:49:10 +0900

    0340

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import FriendList, FriendDetail
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
     path('edit/<int:num>', views.edit, name='edit'),
     path('delete/<int:num>', views.delete, name='delete'),
+    path('list', FriendList.as_view()),
+    path('detail/<int:pk>', FriendDetail.as_view()),
 ]
