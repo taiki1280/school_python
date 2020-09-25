3c83b858824c0e57f04bc98c1b72f11a97f93e6f
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:27:33 +0900

    0328

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Friend
+
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
 
 
 class HelloForm(forms.Form):
