5428c38ba68c2a70309ded627319b18fd5cbc3f6
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-25 10:18:14 +0900

    0320

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,7 +3,10 @@ from .models import Friend
 
 
 def index(request):
-    data = Friend.objects.all().values_list('id', 'name', 'age')
+    num = Friend.objects.all().count()
+    first = Friend.objects.all().first()
+    last = Friend.objects.all().last()
+    data = [num, first, last]
     params = {
         'title': 'Hello',
         'data': data,
