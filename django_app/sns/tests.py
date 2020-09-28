c1ae888b7135f2f656ee6afc559777e360675d55
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-28 10:28:53 +0900

    0514

--- django_app/sns/tests.py
+++ django_app/sns/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SnsTests(TestCase):
+    
+    def test_check(self):
+        x = True
+        self.assertTrue(x)
+        y = 100
+        self.assertGreater(y, 0)
+        arr = [10, 20, 30]
+        self.assertIn(20, arr)
+        nn = None
+        self.assertIsNone(nn)
+
