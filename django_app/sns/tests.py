23c5b6aee212e19e89f610bf317c157ca84f90e2
taiki <52108527+taiki1280@users.noreply.github.com>
2020-09-28 10:42:38 +0900

    0519

--- django_app/sns/tests.py
+++ django_app/sns/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from .models import Group, Message
 
 
@@ -43,17 +45,12 @@ class SnsTests(TestCase):
     def test_check(self):
         usr = User.objects.filter(username='test').first()
 
-        msg = Message.objects.filter(content="test").first()
-        self.assertIs(msg.owner_id, usr.id)
-        self.assertEqual(msg.owner.username, usr.username)
-        self.assertEqual(msg.group.title, 'public')
+        # access to SNS.
+        response = self.client.get(reverse('index'))
+        self.assertIs(response.status_code, 302)
 
-        msgs = Message.objects.filter(content__contains="test").all()
-        self.assertIs(msgs.count(), 2)
-
-        c = Message.objects.all().count()
-        self.assertIs(c, 5)
-
-        msg1 = Message.objects.all().first()
-        msg2 = Message.objects.all().last()
-        self.assertIsNot(msg1, msg2)
+        # login test account and access to SNS.
+        self.client.force_login(usr)
+        response = self.client.get(reverse('index'))
+        self.assertIs(response.status_code, 200)
+        self.assertContains(response, 'this is test message.')
